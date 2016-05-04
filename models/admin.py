b4b9aa07037e6c45895b4b4be8057dc2f6a4e400
HuYong <nghuyong@qq.com>
2016-05-04 22:11:27 +0800

    fix model eerror

--- models/admin.py
+++ models/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username","student_number")
-    search_fields = ("student_number","username")
+    list_display = ("username",)
+    search_fields = ("username",)
 
 
 
