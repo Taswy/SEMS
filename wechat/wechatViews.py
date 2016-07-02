cd6df0ee0a377835892194a4fd5a1c37bbf30c5c
SimpleBrightMan <nghuyong@qq.com>
2016-07-03 02:13:38 +0800

    计算距离用户最近的几个充电点

--- wechat/wechatViews.py
+++ wechat/wechatViews.py
@@ -1,7 +1,9 @@
 # author: HuYong
 #-*- coding: utf-8 -*-
+from math import *
+
 from django.shortcuts import render, redirect, HttpResponse
-from models.models import User, Charge, Node, Account
+from models.models import User, Charge, Node, Account, AmmeterGroup
 import requests
 import json
 AppID = 'wxce660ee67e094937'
@@ -24,6 +26,23 @@ def getUser(request):
     user = User.objects.get(openid=openid)
     return user
 
+
+def calcDistance(Lat_A, Lng_A, Lat_B, Lng_B):
+     ra = 6378.140  # 赤道半径 (km)
+     rb = 6356.755  # 极半径 (km)
+     flatten = (ra - rb) / ra  # 地球扁率
+     rad_lat_A = radians(Lat_A)
+     rad_lng_A = radians(Lng_A)
+     rad_lat_B = radians(Lat_B)
+     rad_lng_B = radians(Lng_B)
+     pA = atan(rb / ra * tan(rad_lat_A))
+     pB = atan(rb / ra * tan(rad_lat_B))
+     xx = acos(sin(pA) * sin(pB) + cos(pA) * cos(pB) * cos(rad_lng_A - rad_lng_B))
+     c1 = (sin(xx) - xx) * (sin(pA) + sin(pB)) ** 2 / cos(xx / 2) ** 2
+     c2 = (sin(xx) + xx) * (sin(pA) - sin(pB)) ** 2 / sin(xx / 2) ** 2
+     dr = flatten / 8 * (c1 - c2)
+     distance = ra * (xx + dr)
+     return distance
 
 #用户绑定
 def bind(request):
@@ -67,4 +86,8 @@ def history(request):
 
 #返回充电站的信息列表
 def AmmeterGroupUrl(request):
-    pass
+    user = getUser(request)
+    group  = AmmeterGroup.objects.all()
+
+
+
