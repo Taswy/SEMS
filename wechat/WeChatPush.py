5035bd4ac81a4412983f14c3a198ba4fe25a7eb0
SimpleBrightMan <nghuyong@qq.com>
2016-07-02 23:54:38 +0800

    add WechatPush

---
+++ wechat/WeChatPush.py
@@ -0,0 +1,24 @@
+# author: HuYong
+# coding=utf-8
+
+
+#充电即将完成推送
+
+def WeChatPush_almostFinish(user):
+    pass
+
+#充电完成推送
+
+def WeChatPush_alreadyFinish(user):
+    pass
+
+#充电完毕本次充电信息推送
+
+def WeChatPush_payFinish(user,charge,account):
+    pass
+
+
+#充电异常推送
+
+def WeChatPush_Exception(user,charge):
+    pass
