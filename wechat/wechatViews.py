ee4e884b8749f9a52c1e26081e75c7a7a05bdd8d
SimpleBrightMan <nghuyong@qq.com>
2016-07-12 00:27:35 +0800

    fix

--- wechat/wechatViews.py
+++ wechat/wechatViews.py
@@ -11,6 +11,9 @@ AppID = 'wxce660ee67e094937'
 AppSecret = '10108b4f9ec7bb9b76f4699087f620e6'
 
 
+
+'''**************************工具方法**************************************************'''
+
 # 由request请求中的code换取openid
 def getOpenid(request):
     code = request.REQUEST.get("code")
@@ -29,7 +32,13 @@ def getUser(request):
     return user
 
 
-# 用户绑定
+'''**************************************************************************************************'''
+
+
+'''*********************************我的**************************************************************'''
+
+
+# 用户绑定情况判断
 def bind(request):
     openid = getOpenid(request)
     context = {"openid": openid}
@@ -41,19 +50,34 @@ def bind(request):
         return render(request, "wechat/regist.html", context)
 
 
-# 返回实时状态
-def state(request):
-    user = getUser(request)
-    charge = Charge.objects.filter(user=user).order_by("-start_time")[0]
-    nodes = Node.objects.filter(time__gt=charge.start_time).order_by("-time")
-    return HttpResponse(nodes[0].voltage_value)
 
+#用户绑定
+def doregist(request):
+    if request.method == 'POST':  # 当提交表单时
+        username = request.POST.get("username", "")
+        student_number = request.POST.get("student_number", "")
+        password = request.POST.get("password", "")
+        count = User.objects.filter(student_number=student_number).count()
+        if count > 0:
+            openid = request.POST.get("openid")
+            new = User(username=username, password=password, student_number=student_number, openid=openid)
+            new.save()
+            return render(request, "wechat/welcome.html", {"openid": openid, "user": new})
+        else:
+            html = '''
+                <head>
+                <meta http-equiv="refresh" content="1;url=/wechat/regist">
+                </head>
+                <h1>请到管理员处登记！</h1>'''
+            return HttpResponse(html)
+    else:
+        html = '''
+            <head>
+            <meta http-equiv="refresh" content="1;url=/wechat/regist">
+            </head>
+            格式错误！'''
+        return HttpResponse(html)
 
-# 反向控制
-def control(request):
-    action = request.REQUEST.get("action")
-    user = getUser(request)
-    charge = Charge.objects.filter(user=user).order_by("-start_time")[0]
 
 
 # 查询消费记录
@@ -71,6 +95,15 @@ def history(request):
     return HttpResponse(content)
 
 
+
+'''**************************************************************************************************'''
+
+
+
+
+'''****************************************充电**************************************************************'''
+
+
 # 返回充电站的信息列表
 def nearby(request):
     user = getUser(request)
@@ -84,53 +117,51 @@ def nearby(request):
         distanceList.append(distance)
     distanceListSort = distanceList[:]
     distanceListSort.sort()
+    resultGroupName = []
+    resultValidNum = []
+    resultDistance = []
     for distance in distanceListSort:
         group = groups[distanceList.index(distance)]
-        content = content + group.ammeterGroup_name.encode("utf-8") + "\t还有" + str(
-            group.valid_number) + "个空位" + "\t距离您" + str(distance) + "m\n"
-    print content
+        resultGroupName.append(group.ammeterGroup_name.encode("utf-8"))
+        resultValidNum.append(group.valid_number)
+        resultDistance.append(distance)
     return HttpResponse(content)
 
 
-def regist(request):
-    openid = request.GET.get("openid")
-    print openid
-    return render(request, "wechat/regist.html", {"openid": openid})
-
-
-def doregist(request):
-    if request.method == 'POST':  # 当提交表单时
-        username = request.POST.get("username", "")
-        student_number = request.POST.get("student_number", "")
-        password = request.POST.get("password", "")
-        count = User.objects.filter(student_number=student_number).count()
-        if count > 0:
-            openid = request.POST.get("openid")
-            if openid == "":
-                new = User(username=username, password=password, student_number=student_number)
-                new.save()
-                # 邮件发送！
-                # from_student_number = settings.DEFAULT_FROM_student_number
-                # context = "恭喜你注册成功！"+"您的账号是："+username+"；您的密码是："+password
-                # send_mail('Welcome', context, from_student_number, [student_number], fail_silently=False)
-                return redirect("/wechat")
-            else:
-                print openid
-                new = User(username=username, password=password, student_number=student_number, openid=openid)
-                new.save()
-                return render(request, "wechat/welcome.html", {"openid": openid, "user": new})
-
-        else:
-            html = '''
-                <head>
-                <meta http-equiv="refresh" content="1;url=/wechat/regist">
-                </head>
-                <h1>请到管理员处登记！</h1>'''
-            return HttpResponse(html)
+# 返回实时状态
+def state(request):
+    user = getUser(request)
+    charge = Charge.objects.filter(user=user).order_by("-start_time")[0]
+    node = Node.objects.filter(time__gt=charge.start_time).order_by("-time")[0]
+    prosess = (node.energy_value / 5 + charge.InitEnergy)*100
+    if prosess > 75:
+        result = 4
+    elif prosess > 50:
+        result = 3
+    elif prosess > 25:
+        result = 2
     else:
-        html = '''
-            <head>
-            <meta http-equiv="refresh" content="1;url=/wechat/regist">
-            </head>
-            格式错误！'''
-        return HttpResponse(html)
+        result = 1
+    return HttpResponse(node[0].voltage_value)
+
+
+# 反向控制
+def control(request):
+    user = getUser(request)
+    charge = Charge.objects.filter(user=user).order_by("-start_time")[0]
+    ammeter = charge.ammeter
+    if request.method == 'GET':
+        status = ammeter.status
+        if status == '0':
+            pass
+        else:
+            pass
+    else:
+        action = request.REQUEST.get("action")
+        if action == "start":
+            ammeter.status = '0'
+        else:
+            ammeter.status = '1'
+
+
+'''**************************************************************************************************'''
